162ff166a0156c0b9441a544ceb7892901652c48
Preeti Vanjani <preeti.r.vanjani@apisero.com>
2022-02-02 09:11:10 -0600

    remove all files

--- src/error-handler/error-code.ts
+++
@@ -1,6 +0,0 @@
-export class ErrorCode {
-    public static readonly Unauthenticated = "Unauthenticated";
-    public static readonly NotFound = "NotFound";
-    public static readonly AsyncError = "AsyncError";
-    public static readonly UnknownError = "UnknownError";
-  }
--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-
-const PORT = process.env.PORT || 7000;
-const server = app.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
-
-export default server;
